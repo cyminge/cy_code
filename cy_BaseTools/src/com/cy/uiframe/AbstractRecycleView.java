e67e9a362bd7b5db2a2aa97c4f411ea8e5aa267a
zhanmin <zhanmin@gionee.com>
2016-12-16 18:10:40 +0800

    cy 添加recycleview jar

---
+++ cy_BaseTools/src/com/cy/uiframe/AbstractRecycleView.java
@@ -0,0 +1,21 @@
+package com.cy.uiframe;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class AbstractRecycleView extends RecyclerView {
+
+	public AbstractRecycleView(Context context) {
+		super(context);
+	}
+
+	public AbstractRecycleView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public AbstractRecycleView(Context arg0, AttributeSet arg1, int arg2) {
+		super(arg0, arg1, arg2);
+	}
+
+}
